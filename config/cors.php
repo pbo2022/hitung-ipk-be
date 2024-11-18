9edd8c7d7fb199b1fdfaece30a77bbb933d5cbe4
pbo2022 <99300556+pbo2022@users.noreply.github.com>
2024-11-18 14:44:56 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://hitung-ipk-fe-2vxx.vercel.app/'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
